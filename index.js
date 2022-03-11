a763ef73ba57aac4e6df9bb534551e1376580cb1
Nicolás Pastrana <pastrana.serra@gmail.com>
2022-03-10 19:14:38 -0500

    First FS test

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const colors = require('colors');
+const fs = require('fs');
 
-console.log('Hola Mundo'.bold.red);
-console.log('Hola'.bold.white, 'Mundo'.dim.gray);
+const fileName = 'data.json';
+const filePath = `data/${fileName}`;
+
+const hola = 'Hola'.bold.white;
+const mundo = 'Mundo'.dim.gray;
+
+let lines;
+try {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  lines = JSON.parse(content);
+} catch (error) {
+  lines = [];
+}
+
+lines.push(`${hola} ${mundo}`);
+
+fs.writeFileSync(filePath, JSON.stringify(lines));
+
+lines.forEach((line) => {
+  console.log(line);
+});
