7c50af7cf47f129b286b242dace163072f0e1918
Nicolás Pastrana <pastrana.serra@gmail.com>
2022-03-10 19:25:12 -0500

    evaluates whether exists the data directory

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const colors = require('colors');
 const fs = require('fs');
 
 const fileName = 'data.json';
-const filePath = `data/${fileName}`;
+const dirPath = 'data';
+const filePath = `${dirPath}/${fileName}`;
 
 const hola = 'Hola'.bold.white;
 const mundo = 'Mundo'.dim.gray;
 
 let lines;
+
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
 try {
   const content = fs.readFileSync(filePath, 'utf-8');
   lines = JSON.parse(content);
